236fa1b23644ae94ffa57aab078f5f8f22c12063
Ricardo <ricardoms1909@gmail.com>
2024-11-30 17:36:43 -0500

    add scheduler AcademicPeriodScheduler

---
+++ outcome-curr-mgmt/src/main/java/co/edu/icesi/dev/outcome_curr_mgmt/service/scheduler/AcademicPeriodScheduler.java
@@ -0,0 +1,37 @@
+package co.edu.icesi.dev.outcome_curr_mgmt.service.scheduler;
+
+import co.edu.icesi.dev.outcome_curr.mgmt.model.stdindto.management.AcadPeriodInDTO;
+import co.edu.icesi.dev.outcome_curr_mgmt.service.management.AcPeriodService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AcademicPeriodScheduler {
+
+    private final AcPeriodService acPeriodService;
+
+    public AcademicPeriodScheduler(AcPeriodService acPeriodService) {
+        this.acPeriodService = acPeriodService;
+    }
+
+    // Ejecutar la consulta periódica cada 30 segundos
+    @Scheduled(fixedRate = 30000)
+    public void performPeriodicQueries() {
+        // Simulación de consulta de todos los periodos académicos
+        acPeriodService.getAllAcademicPeriods();
+    }
+
+    // Ejecutar pruebas periódicas sobre el servicio de creación cada 1 minuto
+    @Scheduled(fixedRate = 60000)
+    public void performPeriodicCreation() {
+        // Crear un nuevo periodo académico para pruebas de carga
+        String acPeriodNameEng = "Academic Period " + System.currentTimeMillis();
+        String setAcPeriodNameSpa = "Periodo Academico " + System.currentTimeMillis();
+        int setAcPeriodNumeric = (int) (Math.random() * 10000);
+
+        AcadPeriodInDTO newAcademicPeriod = new AcadPeriodInDTO(acPeriodNameEng, setAcPeriodNameSpa,
+                setAcPeriodNumeric);
+
+        acPeriodService.addAcademicPeriod(newAcademicPeriod);
+    }
+}
